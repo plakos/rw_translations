ffccab3ccf49f1367f446320c897974c507631ad
Joachim Happel <joachim.happel@gmail.com>
2016-06-20 15:16:44 +0200

    add theme support

--- plugins/rw-toolest.php
+++ plugins/rw-toolest.php
@@ -11,6 +11,7 @@ function rw_fix_correct_plugin_textdomain_dir() {
 		  'bp-docs'
 		, 'bp-group-hierarchy'
 		, 'bbpvotes'
+		, 'buddypress'
 	) ;
 	
 	foreach ($domains as $domain ){
@@ -22,3 +23,23 @@ function rw_fix_correct_plugin_textdomain_dir() {
 	}
 }
 add_action( 'plugins_loaded', 'rw_fix_correct_plugin_textdomain_dir' );
+
+function rw_fix_correct_theme_textdomain_dir() {
+	
+	$locale = get_locale();
+	
+	$domains = array (
+		  'boss'
+		, 'social-learner'
+		
+	) ;
+	
+	foreach ($domains as $domain ){
+		if ( $loaded = load_theme_textdomain( $domain, trailingslashit( WP_LANG_DIR ) .'themes/' . $domain . '-' . $locale . '.mo' , true) ) {
+			return $loaded;
+		} else {
+			load_theme_textdomain( $domain, FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+		}	
+	}
+}
+add_action( 'after_setup_theme', 'rw_fix_correct_theme_textdomain_dir' );
